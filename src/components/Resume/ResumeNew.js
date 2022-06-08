deec3f107a0e4e95deaf778b14c1ccf27cf9ac82
aghosh0605 <aghosh0605@gmail.com>
2022-06-08 12:59:51 +0530

    🐛 Metadata Changed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "./../../Assets/resume.pdf";
+const resumeLink =
+  "https://raw.githubusercontent.com/aghosh0605/portfolio-aghosh0605/main/src/Assets/resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
